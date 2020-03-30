2b91f5da0628e03d0044c38108ef94cc49678b99
Omar Villanueva <o.villanueva@thearcane.com>
2020-03-30 17:03:21 -0600

    Version 3 - advanced child clone

--- src/pages/implicitState/app.js
+++ src/pages/implicitState/app.js
@@ -1,101 +1,90 @@
-import React, { Component } from 'react'
+import React, { Component, Children } from 'react'
 import './sass/main.scss'
 
-class Tabs extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            activeIndex: 0
-        }
-
-        this.selectTabIndex = this.selectTabIndex.bind(this);
+class Card extends Component {
+    state = {
+        activeIndex: 0,
     }
 
-    selectTabIndex(e) {
-        this.setState({ activeIndex:  parseInt(e.target.dataset.index)})
+    selectTabIndex = (e) => {
+        this.setState({ activeIndex: parseInt(e.target.dataset.index) })
     }
-
-    renderTabs() {
-        const { data, disabled } = this.props
-        return data.map((tab, i) => {
-            const isActive = i === this.state.activeIndex
-            const isDisabled = disabled && parseInt(disabled) === i;
-
-            return (
-                <button key={`${tab.label}-${i}`}
-                    data-index={i}
-                    className={`tab${isActive ? ' active' : ''}`}
-                    onClick={this.selectTabIndex}
-                    disabled={isDisabled}
-                >
-                    {tab.label}
-                </button>
-            )
-        })
-    }
-
-    renderPanels () {
-        const {data} = this.props
-        const { activeIndex } = this.state
-        return ( <div>{data[activeIndex].content}</div> )
-    }
-
-
-
 
     render() {
-        const {tabsOnBottom} = this.props;
-
-        const tabs = (
-            <div className="tabs">
-                {this.renderTabs()}
-            </div>
-        )
-
-        const panel = (
-            <div className="panel">
-                {this.renderPanels()}
-            </div>
-        )
+        const children = React.Children.map(this.props.children, (child, i) => {
+            return React.cloneElement(child, {
+                activeIndex: this.state.activeIndex,
+                disabledIndex: this.state.disabledIndex,
+                onSelect: this.selectTabIndex,
+            })
+        })
 
         return (
             <div className="card">
-                {tabsOnBottom ? [ panel, tabs ] : [tabs, panel]}
+                {children}
             </div>
         )
     }
 }
 
-export default class index extends Component {
-    render() {
-        const tabData = [
-            {
-                label: 'Rental',
-                content: (<h2>Rental</h2>)
-            },
-            {
-                label: 'Hotels',
-                content: (<h2>Hotels</h2>)
-            },
-            {
-                label: 'Flights',
-                content: (<h2>Flights</h2>)
-            },
-            {
-                label: 'Restaurnat',
-                content: (<h2>Restaurant</h2>)
-            },
-        ]
+const Tabs = (props) => {
+    const children = React.Children.map(props.children, (child, i) => {
+        return React.cloneElement(child, {
+            index: i,
+            onSelect: props.onSelect,
+            isActive: i === props.activeIndex,
+        })
+    })
 
-        return (
-            <div className="blue-bg">
-                <Tabs
-                disabled={1}
-                data={tabData}
-                tabsOnBottom={true}/>
-            </div>
-        )
-    }
+    return (
+        <div className="tabs">
+            {children}
+        </div>
+    )
+}
+const Tab = ({ isActive, isDisabled, ...rest }) => {
+    return (
+        <button
+            data-index={rest.index}
+            className={`tab${!isDisabled && isActive ? ' active' : ''}`}
+            disabled={isDisabled}
+            onClick={rest.onSelect}
+        > {rest.children} </button>
+    )
 }
 
+const Panels = (props) => {
+    return (
+        <div className="panel">
+            {React.Children.map(props.children, (child, i) => {
+                return props.activeIndex === i ? child : null
+            })}
+        </div>
+    )
+}
+
+const Panel = (props) => (props.children)
+
+export default function index() {
+    return (
+        <div className="blue-bg">
+            <Card>
+               
+                <Panels>
+                    <Panel><h2>Rentals</h2></Panel>
+                    <Panel><h2>Hotels</h2></Panel>
+                    <Panel><h2>Flights</h2></Panel>
+                    <Panel><h2>Restaurants</h2></Panel>
+                    <Panel><h2>Events</h2></Panel>
+                </Panels>
+                <Tabs>
+                    <Tab>Rentals</Tab>
+                    <Tab>Hotels</Tab>
+                    <Tab isDisabled>Flights</Tab>
+                    <Tab>Restaurants</Tab>
+                    <Tab isDisabled>Events</Tab>
+                </Tabs>
+            </Card>
+        </div>
+    )
+}
